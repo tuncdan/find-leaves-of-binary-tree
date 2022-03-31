3b21c3472bdc3f67ccd574eb6ecab45e8962a358
Büşra Tunçdan <busratuncdan@gmail.com>
2022-03-31 17:51:44 +0300

    Update TreeNode.cs

--- TreeNode.cs
+++ TreeNode.cs
@@ -2,14 +2,15 @@
 {
     public class TreeNode
     {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+        
         public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
         {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+            Val = val;
+            Left = left;
+            Right = right;
         }
     }
 }
